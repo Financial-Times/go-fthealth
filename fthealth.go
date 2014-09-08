131255017c77dd835d4f0d64a37d6ac6a01d6a6e
Martin Garton <martin.garton@ft.com>
2014-09-08 16:39:58 +0100

    initial commit

---
+++ fthealth.go
@@ -0,0 +1,69 @@
+package fthealth
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type HealthCheck struct {
+	name        string
+	description string
+	checks      []Check
+}
+
+type Check struct {
+	BusinessImpact   string
+	Name             string
+	PanicGuide       string
+	Severity         uint8 //TODO: enumerate
+	TechnicalSummary string
+	Checker          func() error
+}
+
+func (ch *HealthCheck) health() (result HealthResult) {
+	result.Name = ch.name
+	result.Description = ch.description
+	result.SchemaVersion = 1
+	for _, checker := range ch.checks {
+		result.Checks = append(result.Checks, runChecker(checker))
+	}
+	return
+}
+
+func runChecker(ch Check) CheckResult {
+	result := CheckResult{
+		BusinessImpact:   ch.BusinessImpact,
+		LastUpdated:      time.Now(),
+		Name:             ch.Name,
+		PanicGuide:       ch.PanicGuide,
+		Severity:         ch.Severity,
+		TechnicalSummary: ch.TechnicalSummary,
+	}
+	err := ch.Checker()
+	if err != nil {
+		result.Ok = false
+		result.Output = err.Error()
+	} else {
+		result.Ok = true
+	}
+	return result
+}
+
+func (ch *checkHandler) handle(w http.ResponseWriter, r *http.Request) {
+	health := ch.health()
+	enc := json.NewEncoder(w)
+	err := enc.Encode(health)
+	if err != nil {
+		panic("write this bit")
+	}
+}
+
+type checkHandler struct {
+	HealthCheck
+}
+
+func Handler(name, description string, checks ...Check) func(w http.ResponseWriter, r *http.Request) {
+	ch := &checkHandler{HealthCheck{name, description, checks}}
+	return ch.handle
+}
---
+++ json_structs.go
@@ -0,0 +1,23 @@
+package fthealth
+
+import (
+	"time"
+)
+
+type CheckResult struct {
+	BusinessImpact   string    `json:"businessImpact"`
+	Output           string    `json:"checkOutput"`
+	LastUpdated      time.Time `json:"lastUpdated"`
+	Name             string    `json:"name"`
+	Ok               bool      `json:"ok"`
+	PanicGuide       string    `json:"panicGuide"`
+	Severity         uint8     `json:"severity"`
+	TechnicalSummary string    `json:"technicalSummary"`
+}
+
+type HealthResult struct {
+	Checks        []CheckResult `json:"checks"`
+	Description   string        `json:"description"`
+	Name          string        `json:"name"`
+	SchemaVersion float64       `json:"schemaVersion"`
+}
