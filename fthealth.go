65caf246ac5c252097d9e7c33695f0c8b62ce14e
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-06-26 15:29:39 +0300

    HTML healthcheck response
    Having a more readable output in the case of querying the healthchecks from a browser

--- fthealth.go
+++ fthealth.go
@@ -2,7 +2,9 @@ package fthealth
 
 import (
 	"encoding/json"
+	"html/template"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -52,7 +54,14 @@ func runChecker(ch Check) CheckResult {
 
 func (ch *checkHandler) handle(w http.ResponseWriter, r *http.Request) {
 	health := ch.health()
-    w.Header().Add("Content-Type", "application/json")
+
+	if strings.Contains(r.Header.Get("Accept"), "text/html") {
+		err := writeHTMLResp(w, health)
+		if err == nil {
+			return
+		}
+	}
+	w.Header().Add("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
 	err := enc.Encode(health)
 	if err != nil {
@@ -67,4 +76,37 @@ type checkHandler struct {
 func Handler(name, description string, checks ...Check) func(w http.ResponseWriter, r *http.Request) {
 	ch := &checkHandler{HealthCheck{name, description, checks}}
 	return ch.handle
+}
+
+func writeHTMLResp(w http.ResponseWriter, health HealthResult) error {
+	w.Header().Add("Content-Type", "text/html")
+	t := template.New("healthchecks")
+	t, err := t.Parse(` <!DOCTYPE html>
+                            <head>
+                                <title>Healthchecks</title>
+                            </head>
+
+                            <body>
+                                <h2>Checks</h2>
+                                <ul>
+                                    {{ range $key, $value := . }}
+                                        <li>
+                                            <strong> {{ $value.Name }} </strong>
+                                            <ul>
+                                                <li> <strong> Ok: {{ $value.Ok }} </strong> </li>
+                                                <li> Business impact: {{ $value.BusinessImpact }} </li>
+                                                <li> Output: {{ $value.Output }} </li>
+                                                <li> Last updated: {{ $value.LastUpdated }} </li>
+                                                <li> Panic guide: <a href="{{ $value.PanicGuide }}">{{ $value.PanicGuide }}</a> </li>
+                                                <li> Severity: {{ $value.Severity }} </li>
+                                                <li> Technical summary: {{ $value.TechnicalSummary }} </li>
+                                            </ul>
+                                        </li>
+                                    {{ end }}
+                                </ul>
+                            </body>`)
+	if err == nil {
+		t.Execute(w, health.Checks)
+	}
+	return err
 }
